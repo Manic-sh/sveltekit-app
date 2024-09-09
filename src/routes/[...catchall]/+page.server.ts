8ede6b8ab6ea9859b119ca4ef155fc05520ccb83
TheIndiMonk <theindimonk@gmail.com>
2024-09-09 23:14:38 +0530

    ISR

--- src/routes/[...catchall]/+page.server.ts
+++ src/routes/[...catchall]/+page.server.ts
@@ -1,18 +1,35 @@
-import { fetchOneEntry, getBuilderSearchParams } from '@builder.io/sdk-svelte';
-import { BUILDER_PUBLIC_API_KEY } from '../../apiKey';
+import {
+  fetchOneEntry,
+  getBuilderSearchParams,
+  fetchEntries,
+} from "@builder.io/sdk-svelte";
+import { BUILDER_PUBLIC_API_KEY } from "../../apiKey";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load(event) {
-	// fetch your Builder content
-	const content = await fetchOneEntry({
-		model: 'page',
-		apiKey: BUILDER_PUBLIC_API_KEY,
-		options: getBuilderSearchParams(event.url.searchParams),
-		userAttributes: {
-			urlPath: event.url.pathname || '/'
-		},
-		enrich: true,
-	});
+  // fetch your Builder content
+  const content = await fetchOneEntry({
+    model: "page",
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    options: getBuilderSearchParams(event.url.searchParams),
+    userAttributes: {
+      urlPath: event.url.pathname || "/",
+    },
+    enrich: true,
+  });
 
-	return { content };
+  const entries = await fetchEntries({
+    model: "page",
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    enrich: true,
+    query: {
+      id: "5af62fa81a1b4c9b907e6411189228eb",
+      data: {
+        score: {$eq: 99},
+      },
+    },
+  });
+  console.log(".......", entries);
+
+  return { content };
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-vercel";
 
-/** @type {import('@sveltejs/kit').Config} */
+import preprocess from "svelte-preprocess";
+
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	isr : {
-		expiration: 60,
-	}
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
+
+  kit: {
+    adapter: adapter({
+      isr: {
+        expiration: 60, // Cache expiration in seconds (1 minute)
+      },
+    }),
+    alias: {
+      $src: "./src",
+      $lib: "./src/lib",
+    },
+  },
 };
 
 export default config;
